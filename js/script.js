9852b11046da81d165883bba9740cb00922e506b
D_Sykes <Itachi_15_90@yahoo.com>
2023-11-24 00:09:49 -0600

    Added js to change background

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var body = document.querySelector(".happy");
+var dropDown = document.querySelector("#moods");
+var mood = "happy";
+
+
+dropDown.addEventListener("change", function(e){
+ mood = e.target.value;
+
+    if (mood === "sad"){
+        body.classList.remove("happy");
+        body.classList.remove("passionate");
+        body.classList.add("sad");
+    } else if (mood === "passionate"){
+        body.classList.remove("sad");
+        body.classList.remove("happy");
+        body.classList.add("passionate");
+    } else {
+        body.classList.remove("passionate");
+        body.classList.remove("sad");
+        body.classList.add("happy");
+    }
+});
